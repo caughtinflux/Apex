edd01d9ec28e4abe4a6a1a3a6b71232737ccd6e4
caughtinflux <caughtinflux@icloud.com>
2013-05-20 00:25:56 +0530

    Add new macro for alert

--- STKConstants.h
+++ STKConstants.h
@@ -3,22 +3,31 @@
 
 #import <Foundation/Foundation.h>
 #import <CoreGraphics/CGBase.h>
+#import <CoreFoundation/CoreFoundation.h>
+#import <CoreFoundation/CFUserNotification.h>
 
 #ifdef DEBUG
-	#define DLog(fmt, ...) NSLog((@"STK: %s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
-	#define CLog(fmt, ...) NSLog((@"STK: " fmt), ##__VA_ARGS__)
+    #define DLog(fmt, ...) NSLog((@"STK: %s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
+    #define CLog(fmt, ...) NSLog((@"STK: " fmt), ##__VA_ARGS__)
 #else
-	#define DLog(...)
-	#define CLog(...)
+    #define DLog(...)
+    #define CLog(...)
 #endif
 
 #define BOOL_TO_STRING(b) (b ? @"YES" : @"NO")
 
 #define kPrefPath [NSString stringWithFormat:@"%@/Library/Preferences/Acervos/com.a3tweaks.%@.plist", NSHomeDirectory(), STKTweakName]
+#define kTargetDistance ((UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) ? 76.0f : 176.0f)
+
 
 #define EXECUTE_BLOCK_AFTER_DELAY(delayInSeconds, block) (dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC)), dispatch_get_main_queue(), block))
 
-#define kTargetDistance ((UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) ? 76.0f : 176.0f)
+#define SHOW_USER_NOTIFICATION(title, message, dismissButtonTitle) \
+                               NSDictionary *fields = @{(id)kCFUserNotificationAlertHeaderKey        : title, \
+                                                        (id)kCFUserNotificationAlertMessageKey       : message, \
+                                                        (id)kCFUserNotificationDefaultButtonTitleKey : dismissButtonTitle}; \
+                               CFUserNotificationRef notificationRef = CFUserNotificationCreate(kCFAllocatorDefault, 0, kCFUserNotificationNoteAlertLevel, NULL, (CFDictionaryRef)fields); \
+                               CFRelease(notificationRef)
 
 @class NSString, SBIconListView, SBIcon;
 
@@ -26,26 +35,26 @@
 extern "C" {
 #endif
 
-	extern NSString * const STKTweakName;
-	extern NSString * const STKEditingStateChangedNotification;
-	extern NSString * const STKStackClosingEventNotification; // This notification is posted when something happens to make the stack close
-	
-	extern NSString * const SBLockStateChangeNotification;
+    extern NSString * const STKTweakName;
+    extern NSString * const STKEditingStateChangedNotification;
+    extern NSString * const STKStackClosingEventNotification; // This notification is posted when something happens to make the stack close
+    
+    extern NSString * const SBLockStateChangeNotification;
 
-	// Function to translate a number from one range to another
-	// For instance 248 in the range [0, 320] -> something 0.0 -> 0.1
-	extern inline double STKScaleNumber(double numToScale, double prevMin, double prevMax, double newMin, double newMax);
+    // Function to translate a number from one range to another
+    // For instance 248 in the range [0, 320] -> something 0.0 -> 0.1
+    extern inline double STKScaleNumber(double numToScale, double prevMin, double prevMax, double newMin, double newMax);
 
-	// Wrapper functions
-	extern inline double __attribute__((overloadable)) STKAlphaFromDistance(double distance);
-	extern inline double __attribute__((overloadable)) STKAlphaFromDistance(double distance, BOOL isGhostly);
+    // Wrapper functions
+    extern inline double __attribute__((overloadable)) STKAlphaFromDistance(double distance);
+    extern inline double __attribute__((overloadable)) STKAlphaFromDistance(double distance, BOOL isGhostly);
 
-	extern SBIconListView * STKListViewForIcon(SBIcon *icon);
+    extern SBIconListView * STKListViewForIcon(SBIcon *icon);
 
-	extern CGFloat STKGetCurrentTargetDistance(void);
-	extern void    STKUpdateTargetDistanceInListView(SBIconListView *listView);
+    extern CGFloat STKGetCurrentTargetDistance(void);
+    extern void    STKUpdateTargetDistanceInListView(SBIconListView *listView);
 
-	extern NSUInteger STKGetCPUFrequency(void);
+    extern NSUInteger STKGetCPUFrequency(void);
 
 #ifdef __cplusplus 
 }
