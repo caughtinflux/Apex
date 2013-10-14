f88bf662a4271bd998923c47228a28c1e2ac2676
caughtinflux <caughtinflux@icloud.com>
2013-10-14 22:15:54 +0530

    Indentation

--- STKRecognizerDelegate.m
+++ STKRecognizerDelegate.m
@@ -7,20 +7,19 @@
 
 - (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer
 {
-	// Only allow recognition with main content view and folder recognizer view.
     return ((otherGestureRecognizer == [[objc_getClass("SBIconController") sharedInstance] scrollView].panGestureRecognizer) ||
-    		([otherGestureRecognizer.view isKindOfClass:[UIScrollView class]] && [otherGestureRecognizer.view.superview isKindOfClass:objc_getClass("FEGridFolderView")]));
+            ([otherGestureRecognizer.view isKindOfClass:[UIScrollView class]] && [otherGestureRecognizer.view.superview isKindOfClass:objc_getClass("FEGridFolderView")]));
 
 }
 
 - (BOOL)gestureRecognizer:(UIGestureRecognizer *)recognizer shouldReceiveTouch:(UITouch *)touch
 {
-	Class superviewClass = [recognizer.view.superview class];
-	if ([superviewClass isKindOfClass:[objc_getClass("SBDockIconListView") class]]) {
-		return NO;
-	}
+    Class superviewClass = [recognizer.view.superview class];
+    if ([superviewClass isKindOfClass:[objc_getClass("SBDockIconListView") class]]) {
+        return NO;
+    }
 
-	return YES;
+    return YES;
 }
 
 @end
