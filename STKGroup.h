ece70bf76e083f4420654060c641278c94af83ff
Aditya KD <adityavkd@icloud.com>
2014-01-14 12:57:37 +0530

    Implement SBIconViewDelegate

--- STKGroup.h
+++ STKGroup.h
@@ -1,16 +1,28 @@
 #import <Foundation/Foundation.h>
 #import "STKConstants.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+    extern NSString * const STKGroupCentralIconKey;
+    extern NSString * const STKGroupLayoutKey;
+    extern NSString * const STKGroupCoordinateKey;
+#ifdef __cplusplus
+}
+#endif
+
+
 @class STKGroupView, STKGroupLayout;
 @protocol STKGroupObserver;
-@interface STKGroup : NSObject
+@interface STKGroup : NSObject <SBIconViewDelegate>
 
 - (instancetype)initWithCentralIcon:(SBIcon *)icon layout:(STKGroupLayout *)layout;
+- (instancetype)initWithDictionary:(NSDictionary *)repr;
 
 @property (nonatomic, retain) SBIcon *centralIcon;
 @property (nonatomic, readonly) STKGroupLayout *layout;
 @property (nonatomic, readonly) NSDictionary *dictionaryRepresentation;
-@property (nonatomic, readonly) STKGroupView *view; 
+@property (nonatomic, assign) SBIconCoordinate lastKnownCoordinate;
 
 - (void)addObserver:(id<STKGroupObserver>)observer;
 - (void)removeObserver:(id<STKGroupObserver>)observer;
--- STKGroup.m
+++ STKGroup.m
@@ -5,62 +5,106 @@
 #import <SpringBoard/SpringBoard.h>
 
 NSString * const STKGroupCentralIconKey = @"STKGroupCentralIcon";
-NSString * const STKGroupLayoutKey = @"STKGroupLayout";
+NSString * const STKGroupLayoutKey      = @"STKGroupLayout";
+NSString * const STKGroupCoordinateKey  = @"STKLastKnownCoordinate";
 
 @implementation STKGroup
 {
-	STKGroupLayout *_layout;
-	STKGroupView *_view;
-	NSHashTable *_observers;
+    STKGroupLayout *_layout;
+    STKGroupView *_view;
+    NSHashTable *_observers;
 }
 
 - (instancetype)initWithCentralIcon:(SBIcon *)icon layout:(STKGroupLayout *)layout
 {
-	if ((self = [super init])) {
-		_centralIcon = [icon retain];
-		_layout = [layout retain];
-		_observers = [[NSHashTable alloc] initWithOptions:NSHashTableWeakMemory capacity:0];
-		_view = [[STKGroupView alloc] initWithGroup:self];
-	}
-	return self;
+    if ((self = [super init])) {
+        _centralIcon = [icon retain];
+        _layout = [layout retain];
+        _observers = [[NSHashTable alloc] initWithOptions:NSHashTableWeakMemory capacity:0];
+    }
+    return self;
+}
+
+- (instancetype)initWithDictionary:(NSDictionary *)repr
+{
+    NSParameterAssert([repr allKeys].count > 0);
+    if ((self = [super init])) {
+        NSString *iconID = repr[STKGroupCentralIconKey];
+        _centralIcon = [[(SBIconModel *)[[CLASS(SBIconController) sharedInstance] model] expectedIconForDisplayIdentifier:iconID] retain];
+        _layout = [[STKGroupLayout alloc] initWithIdentifierDictionary:repr[STKGroupLayoutKey]];
+        _lastKnownCoordinate = STKCoordinateFromDictionary(repr[STKGroupCoordinateKey]);
+    }
+    return self;
 }
 
 - (void)dealloc
 {
-	_view.group = nil;
-	[_view release];
-	[_layout release];
-	[_observers removeAllObjects];
-	[_observers release];
-	[super dealloc];
+    _view.group = nil;
+    [_view release];
+    [_layout release];
+    [_observers removeAllObjects];
+    [_observers release];
+    [_centralIcon release];
+    [super dealloc];
 }
 
 - (STKGroupLayout *)layout
 {
-	return _layout;
+    return _layout;
 }
 
 - (NSDictionary *)dictionaryRepresentation
 {
-	return @{
-		STKGroupCentralIconKey: [_centralIcon leafIdentifier] ?: @"",
-		STKGroupLayoutKey: [_layout identifierDictionary] ?: @{}
-	};
+    return @{
+        STKGroupCentralIconKey: [_centralIcon leafIdentifier] ?: @"",
+        STKGroupLayoutKey: [_layout identifierDictionary] ?: @{},
+        STKGroupCoordinateKey: STKDictionaryFromCoordinate(_lastKnownCoordinate)
+    };
 }
 
 - (void)addObserver:(id<STKGroupObserver>)observer
 {
-	[_observers addObject:observer];
+    [_observers addObject:observer];
 }
 
 - (void)removeObserver:(id<STKGroupObserver>)observer
 {
-	[_observers removeObject:observer];		
+    [_observers removeObject:observer];     
 }
 
 - (SBIconView *)_iconViewForIcon:(SBIcon *)icon
 {
-	return [[CLASS(SBIconViewMap) homescreenMap] mappedIconViewForIcon:icon];
+    return [[CLASS(SBIconViewMap) homescreenMap] mappedIconViewForIcon:icon];
+}
+
+- (BOOL)iconViewDisplaysCloseBox:(SBIconView *)iconView
+{
+    return [[CLASS(SBIconController) sharedInstance] iconViewDisplaysBadges:iconView];
+}
+
+- (BOOL)iconViewDisplaysBadges:(SBIconView *)iconView
+{
+    return [[CLASS(SBIconController) sharedInstance] iconViewDisplaysBadges:iconView];
+}
+
+- (BOOL)icon:(SBIconView *)iconView canReceiveGrabbedIcon:(SBIcon *)grabbedIcon
+{
+    return NO;
+}
+
+- (void)iconTapped:(SBIconView *)iconView
+{
+    [iconView.icon launchFromLocation:SBIconLocationHomeScreen];
+}
+
+- (BOOL)iconShouldAllowTap:(SBIconView *)iconView
+{
+    return YES;
+}
+
+- (void)iconHandleLongPress:(SBIconView *)iconView
+{
+    // TODO Editing?
 }
 
 @end
