1155fecce0ef92b33b35456700680c0093f619ca
Aditya KD <aditya@adityakd.me>
2014-04-29 16:21:12 +0530

    Fix icons not launching. Such derp. Much stupid

--- STKGroupController.m
+++ STKGroupController.m
@@ -490,7 +490,7 @@
         [self _showSelectionViewForIconView:iconView];
     }
     else {
-        [iconView.icon launchFromLocation:(iconView.groupView ? SBIconLocationHomeScreen : SBIconLocationSwitcher)];
+        [iconView.icon launchFromLocation:SBIconLocationHomeScreen];
         if ([STKPreferences sharedPreferences].shouldCloseOnLaunch) {
             [activeGroupView close];
         }
