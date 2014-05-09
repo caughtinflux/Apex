ecefa54fc6f14db0c6f46d0ce813df198a2fc540
Aditya KD <aditya@adityakd.me>
2014-05-09 23:36:12 +0530

    proto-what!?

--- STKIconViewRecycler.h
+++ STKIconViewRecycler.h
@@ -1,7 +1,6 @@
 #import <Foundation/Foundation.h>
 #import "STKConstants.h"
 
-@protocol STKIconViewRecyclerDelegate;
 @class SBIcon, SBIconView;
 @interface STKIconViewRecycler : NSObject <STKIconViewSource>
 - (id)iconViewForIcon:(SBIcon *)icon;
